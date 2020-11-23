f9f95fc8d98ae1c67c3c923fcd80e09d14661b2c
kzaw <kzawadzki@wp.pl>
2020-11-23 17:31:27 +0100

    ver 3

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,4 @@
-public class Book {
+ class Book {
 
     String title ;
     String author ;
@@ -6,6 +6,22 @@ public class Book {
     int pages ;
     String publisher ;
     String isbn ;
+
+    Book(String booktitle , String bookauthor , int bookreleaseDate ,  int bookpages ,  String bookpublisher ,  String bookisbn )
+    {
+title=booktitle;
+author=bookauthor;
+releaseDate=bookreleaseDate;
+pages=bookpages;
+publisher=bookpublisher;
+isbn=bookisbn;
+    }
+
+
+     void prontInfo() {
+       String info=title +" "+        author+" "+        releaseDate+" "+        pages+" "+        publisher+" "+        isbn;
+        System.out.println(info);
+    }
 }
 
 
--- src/Library.java
+++ src/Library.java
@@ -1,23 +1,15 @@
-public class Library {
-    public static void main(String[] args) {
-        final String appName = "Biblioteka v0.2";
+ class Library {
+     public static void main(String[] args) {
+        final String appName = "Biblioteka v0.3";
 
-        Book book1=new Book();
-
-        book1.title = "W pustyni i w puszczy";
-        book1.author = "Henryk Sienkiewicz";
-        book1.releaseDate = 2010;
-        book1.pages = 296;
-        book1.publisher = "Greg";
-        book1.isbn = "9788373271890";
+        Book book1=new Book("W pustyni i w puszczy","Henryk Sienkiewicz",2010,296,"Greg","9788373271890");
+        Book book2=new Book("ABC gotowania","Jan Kowalski",2011,29,"ABC","123353453466");
+        Book book3=new Book("Hitman","Henryk Sienkiewicz",2015,333,"Greg","3453452346443");
 
         System.out.println(appName);
-        System.out.println("Książki dostępne w bibliotece:");
-        System.out.println(book1.title);
-        System.out.println(book1.author);
-        System.out.println(book1.releaseDate);
-        System.out.println(book1.pages);
-        System.out.println(book1.publisher);
-        System.out.println(book1.isbn);
+        System.out.println("Książki dostępne w bibliotece");
+        book1.prontInfo();
+        book2.prontInfo();
+        book3.prontInfo();
     }
 }
